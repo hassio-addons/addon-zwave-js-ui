9ca951197489e11c368834b3aff594c11cb97e29
Franck Nijhof <git@frenck.dev>
2021-01-28 21:39:10 +0100

    Initiate default configuration on first run

---
+++ zwavejs2mqtt/rootfs/etc/cont-init.d/configuration.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Z-Wave JS to MQTT
+# Creates store folder if it doesn't exists yet
+# ==============================================================================
+declare gateway
+declare host="core-mosquitto"
+declare mqtt
+declare password=""
+declare port="1883"
+declare username=""
+declare zwave
+
+if bashio::fs.directory_exists "/data/store"; then
+  bashio::exit.ok
+fi
+
+mkdir /data/store || bashio::exit.nok "Could not create data store."
+
+gateway=$(bashio::var.json \
+  "payloadType" "^0" \
+  "type" "^0" \
+  hassDiscovery "^false" \
+  logEnabled "^true" \
+  logLevel info \
+  logToFile "^false" \
+  nodeNames "^true" \
+)
+
+if bashio::services.available "mqtt"; then
+  host=$(bashio::services "mqtt" "host")
+  password=$(bashio::services "mqtt" "password")
+  port=$(bashio::services "mqtt" "port")
+  username=$(bashio::services "mqtt" "username")
+fi
+
+mqtt=$(bashio::var.json \
+  auth "^true" \
+  disabled "^true" \
+  host "${host}" \
+  name "Mosquitto Add-on" \
+  password "${password}" \
+  port "^${port}" \
+  username "${username}" \
+)
+
+zwave=$(bashio::var.json \
+  commandsTimeout "^30" \
+  logLevel info \
+  logToFile "^false" \
+  networkKey "" \
+  port "" \
+  serverEnabled "^true" \
+  serverPort "^3000" \
+)
+
+bashio::var.json \
+  gateway "^${gateway}" \
+  mqtt "^${mqtt}" \
+  zwave "^${zwave}" \
+  > /data/store/settings.json
--- zwavejs2mqtt/rootfs/etc/cont-init.d/folders.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Home Assistant Community Add-on: Z-Wave JS to MQTT
-# Creates store folder if it doesn't exists yet
-# ==============================================================================
-if ! bashio::fs.directory_exists "/data/store"; then
-  mkdir /data/store
-fi
