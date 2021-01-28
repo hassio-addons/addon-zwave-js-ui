c937fa42b0b082b80e42cdb9bad0a8a27d75cdc0
Franck Nijhof <frenck@addons.community>
2021-01-28 18:50:52 +0100

    Add initial add-on code (#1)

---
+++ zwavejs2mqtt/rootfs/etc/cont-init.d/nginx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Z-Wave JS to MQTT
+# Configures NGINX for use with this add-on.
+# ==============================================================================
+bashio::var.json \
+    entry "$(bashio::addon.ingress_entry)" \
+    | tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
---
+++ zwavejs2mqtt/rootfs/etc/nginx/templates/ingress.gtpl
@@ -0,0 +1,14 @@
+server {
+    listen 8099 default_server;
+
+    include /etc/nginx/includes/server_params.conf;
+    include /etc/nginx/includes/proxy_params.conf;
+
+    location / {
+        allow   172.30.32.2;
+        deny    all;
+
+        proxy_pass http://backend;
+        proxy_set_header X-External-Path {{ .entry }};
+    }
+}
